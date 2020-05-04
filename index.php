355923cb67ffe7ea4bcd8cc55eb063f3e57b39d2
turnerchristian <christianturner@hotmail.co.uk>
2020-05-04 16:22:05 +0100

    Basics of main page done

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Armadillo Estates</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css" type="text/css">
+</head>
+<body>
+
+<div class="container">
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30"
+                 class="d-inline-block align-top" alt="">
+        </a>
+        <h2>Armadillo Estates</h2>
+        <h2></h2>
+    </nav>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-4 text-center mb-4">
+            <div class="card">
+                <h5 class="card-title">Hill Farm, Plough Hill Road</h5>
+                <img class="card-img-top" src="https://placehold.it/1920x1080" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">For Sale</p>
+                    <p class="card-text">Available!</p>
+                    <a href="#" class="btn btn-primary">View more...</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 text-center mb-4">
+            <div class="card">
+                <h5 class="card-title">Hill Farm, Plough Hill Road</h5>
+                <img class="card-img-top" src="https://placehold.it/1920x1080" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">For Sale</p>
+                    <p class="card-text">Available!</p>
+                    <a href="#" class="btn btn-primary">View more...</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 text-center mb-4">
+            <div class="card">
+                <h5 class="card-title">Hill Farm, Plough Hill Road</h5>
+                <img class="card-img-top" src="https://placehold.it/1920x1080" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">For Sale</p>
+                    <p class="card-text">Available!</p>
+                    <a href="#" class="btn btn-primary">View more...</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 text-center mb-4">
+            <div class="card">
+                <h5 class="card-title">Hill Farm, Plough Hill Road</h5>
+                <img class="card-img-top" src="https://placehold.it/1920x1080" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">For Sale</p>
+                    <p class="card-text">Available!</p>
+                    <a href="#" class="btn btn-primary">View more...</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 text-center mb-4">
+            <div class="card">
+                <h5 class="card-title">Hill Farm, Plough Hill Road</h5>
+                <img class="card-img-top" src="https://placehold.it/1920x1080" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">For Sale</p>
+                    <p class="card-text">Available!</p>
+                    <a href="#" class="btn btn-primary">View more...</a>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+
+
+</body>
+</html>
