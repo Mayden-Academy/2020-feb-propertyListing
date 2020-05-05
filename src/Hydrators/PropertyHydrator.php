e777f634be724d2614862b9c6d3b4d7c96463155
Bradley <bradley.purdue+mayden@playsportsnetwork.com>
2020-05-05 16:23:40 +0100

    Added a query for all complete listings.

--- src/Hydrators/PropertyHydrator.php
+++ src/Hydrators/PropertyHydrator.php
@@ -7,6 +7,7 @@ namespace ArmadilloEstates\Hydrators;
 use ArmadilloEstates\Collections\PropertyCollection;
 use ArmadilloEstates\Entities\PropertyEntity;
 use ArmadilloEstates\Interfaces\IPropertyCollection;
+use ArmadilloEstates\Interfaces\IPropertyEntity;
 
 class PropertyHydrator
 {
@@ -24,8 +25,21 @@ SELECT `address1`, `address2`, `town`, `typeName`, `statusName`, `properties`.`i
 JOIN `types` ON `properties`.`propertyType` = `types`.`typeId` 
 JOIN `status` ON `properties`.`status` = `status`.`statusId`;
 ");
+
         $result = $query->setFetchMode(\PDO::FETCH_CLASS, PropertyEntity::class);
 
         return new PropertyCollection($query->fetchAll());
+    }
+
+    public function getCompleteListingById(string $id): IPropertyEntity {
+        $query = $this->database->prepare("
+        SELECT `description`, `agentRef`, `postcode`, `price`, `beds`, `address1`, `address2`, `town`, `typeName`, `statusName`, `properties`.`id`,  `image` FROM `properties` 
+JOIN `types` ON `properties`.`propertyType` = `types`.`typeId` 
+JOIN `status` ON `properties`.`status` = `status`.`statusId` WHERE `properties`.`id`=?;
+        ");
+        $query->execute([$id]);
+        $result = $query->setFetchMode(\PDO::FETCH_CLASS, PropertyEntity::class);
+
+        return $query->fetch();
     }
 }
