0daca4074986a340858d2bb7cccec7db0b90ae52
Dani Catterill <danicatterill@gmail.com>
2020-05-05 14:07:19 +0100

    Added ViewHelpers directory and viewHelper class, added functionality to display all on main page.

---
+++ src/ViewHelpers/PropertyViewHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace ArmadilloEstates\ViewHelpers;
+
+
+use ArmadilloEstates\Interfaces\IPropertyCollection;
+
+abstract class PropertyViewHelper
+{
+    public static function displayAll(IPropertyCollection $propertyCollection): string
+    {
+        $stringToDisplay = "";
+        foreach ($propertyCollection->getAllProperties() as $property) {
+            $stringToDisplay .= '<div class="col-4 text-center mb-4"><div class="card">';
+            $stringToDisplay .= '<h5 class="card-title pt-3">' . $property->getAddress1() . ',' . $property->getTown() . '</h5>';
+            $stringToDisplay .= '<img class="card-img-top" src="https://placehold.it/1920x1080" alt="Card image cap">';
+            $stringToDisplay .= '<div class="card-body">';
+            $stringToDisplay .= '<p class="card-text">' . $property->getTypeName() . '</p>';
+            $stringToDisplay .= '<p class="card-text">' . $property->getStatusName() . '</p>';
+            $stringToDisplay .= '<a href="#" class="btn btn-primary">View more...</a>';
+            $stringToDisplay .= '</div></div></div>';
+        }
+
+        return $stringToDisplay;
+    }
+}
