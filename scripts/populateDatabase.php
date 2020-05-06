8a8b527adc77a4033513edc6e1dd30a128044459
Dani Catterill <danicatterill@gmail.com>
2020-05-06 12:31:31 +0100

    commited for lunch - changing 3 foreach loops into 1

--- scripts/populateDatabase.php
+++ scripts/populateDatabase.php
@@ -4,28 +4,37 @@ $propertyUrl = "https://dev.maydenacademy.co.uk/resources/property-feed/properti
 $statusUrl = "https://dev.maydenacademy.co.uk/resources/property-feed/statuses.json";
 $typeUrl = "https://dev.maydenacademy.co.uk/resources/property-feed/types.json";
 
-$database = getArmadilloEstates();
+$database = connectToDatabase();
 
-$propertyArray = getApiData($propertyUrl);
-$statusArray = getApiData($statusUrl);
-$typeArray = getApiData($typeUrl);
+$allProperties = getApiData($propertyUrl);
+$allStatuses = getApiData($statusUrl);
+$allTypes = getApiData($typeUrl);
 
-$database->query("TRUNCATE TABLE `properties`");
-foreach ($propertyArray as $property) {
-    insertIntoPropertiesTable($database, $property);
+$databaseData = [['data'=>$allProperties, 'tableName'=>'properties'],
+    ['data'=>$allStatuses, 'tableName'=>'status'],
+    ['data'=>$allTypes, 'tableName'=>'types']];
+
+foreach ($databaseData as $tableData) {
+    $query = $database->query("TRUNCATE TABLE `$tableData['tableName']`");
+    switch ($tableData['tableName']) {
+        case 'properties':
+            foreach ($tableData['data'] as $data) {
+                insertIntoPropertiesTable($database, $data);
+            }
+            break;
+        case 'status':
+            foreach ($allStatuses as $status) {
+                insertIntoStatusTable($database, $status);
+            }
+            break;
+        case 'types':
+            foreach ($allTypes as $type) {
+                insertIntoTypesTable($database, $type);
+            }
+    }
 }
 
-$database->query("TRUNCATE TABLE `status`");
-foreach ($statusArray as $status) {
-    insertIntoStatusTable($database, $status);
-}
-
-$database->query("TRUNCATE TABLE `types`");
-foreach ($typeArray as $type) {
-    insertIntoTypesTable($database, $type);
-}
-
-function getApiData($url)
+function getApiData(string $url): array
 {
     $ch = curl_init();
 
@@ -40,7 +49,7 @@ function getApiData($url)
     return $result;
 }
 
-function getArmadilloEstates(): PDO
+function connectToDatabase(): PDO
 {
     $db = new PDO('mysql:host=DB;dbname=armadilloEstates', 'root', 'password');
     return $db;
@@ -89,4 +98,5 @@ function insertIntoTypesTable(PDO $db, array $typeData): bool
     $query = $db->prepare("INSERT INTO `types` (`typeId`, `typeName`) VALUES (:ID, :TYPE_NAME);");
     return $query->execute($typeData);
 }
+
 
