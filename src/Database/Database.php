472d68212e1bf9bb097ae4b436ad8d1ce83eba71
Vinc <vinc_944@hotmail.com>
2020-05-06 14:22:42 +0100

    Added constants for connection parameters

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -5,9 +5,14 @@ namespace ArmadilloEstates\Database;
 
 abstract class Database
 {
+    private const HOST = "DB";
+    private const USERNAME = "root";
+    private const PASSWORD = "password";
+    private const DBNAME = "armadilloEstates";
+
     public static function connect(): \PDO
     {
-        $db = new \PDO('mysql:host=DB;dbname=armadilloEstates', 'root', 'password');
+        $db = new \PDO('mysql:host=' . self::HOST . ';dbname=' . self::DBNAME,self::USERNAME,self::PASSWORD);
         $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
 
         return $db;
